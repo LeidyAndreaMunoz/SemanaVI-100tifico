b633f0202a7cfcac2f3f32068b819f4fe0ccddab
Leidy Muñoz <leidy.munoz@alteru.co>
2021-06-29 16:57:25 -0500

    Se agregan los estilos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,13 @@ const Home = async () => {
   const view = `
     <div class="Characters">
       ${characters.results.map(character => `
-      <article class="Character-item">
-        <a href="#/${character.id}/">
-          <img src="${character.image}" alt="${character.name}">
-          <h2>${character.name}</h2>
-        </a>
-      </article>
-    `).join('')}
+        <article class="Character-item">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+      `).join('')}
     </div>
   `;
   return view;
